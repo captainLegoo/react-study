1862c3913eed75b55e9206c9c8ad882c0c9469fa
captaindeng12138 <captaindeng12138@gmail.com>
2024-06-11 15:42:29 +0100

    JSX, Components, Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App"> 
+    <User name="Kyle" age={21} email="admin@admin.com"/>
+    <User name="admin" age={31} email="admin@admin.com"/>
+    <User name="Jack" age={12} email="admin@admin.com"/>
+    <Job salary={10000} position="Developer" company="Facebook"/>
+    <Job salary={20000} position="Developer" company="Google"/>
+    <Job salary={30000} position="Developer" company="Amazon"/>
+  </div>
+  );
+}
+
+const getName = () => {
+  return "Kyle";
+}
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  );
+}
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
     </div>
   );
 }
