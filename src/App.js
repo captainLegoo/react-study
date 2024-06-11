e538164438247b25ac0f1486949196101828bf81
captaindeng12138 <captaindeng12138@gmail.com>
2024-06-11 16:46:08 +0100

    3.Ternary Operators, Lists in React, Css in React

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
 import './App.css';
+import {User} from './User';
 
 function App() {
+  // Ternary Operators
+  // const age  = 11;
+  // const isGreen = false;
+
+  // return (
+  // <div className="App">
+  //   {age >=18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
+  //   <h1 style={{color: isGreen ? "green" : "red" }}> THIS HAS COLOR </h1>
+
+  //   {isGreen && <button>THIS IS A BUTTON</button>}
+  // </div>
+  // );
+
+  const names = ["Kyle", "John", "Eric", "Sally", "Jane", "Mark"];
+
+  const users = [
+    {name: "Kyle", age: 27},
+    {name: "John", age: 17},
+    {name: "Eric", age: 21},
+  ];
+
+  const planets = [
+    {name: "Mars", isGasPlanet: false},
+    {name: "Jupiter", isGasPlanet: true},
+    {name: "Venus", isGasPlanet: false},
+    {name: "Earth", isGasPlanet: false},
+    {name: "Saturn", isGasPlanet: true},
+    {name: "Mercury", isGasPlanet: false}
+  ];
 
   return (
-  <div className="App"> 
-    <User name="Kyle" age={21} email="admin@admin.com"/>
-    <User name="admin" age={31} email="admin@admin.com"/>
-    <User name="Jack" age={12} email="admin@admin.com"/>
-    <Job salary={10000} position="Developer" company="Facebook"/>
-    <Job salary={20000} position="Developer" company="Google"/>
-    <Job salary={30000} position="Developer" company="Amazon"/>
-  </div>
-  );
-}
+    <div className="App">
+      {/* {names.map((name, index) => {
+        return <h1 key={index}> {name} </h1>;
+      })} */}
 
-const getName = () => {
-  return "Kyle";
-}
+      {/* {users.map((user, key) => {
+        // return <div> {user.name} {user.age} </div>;
+        return <User name={user.name} age={user.age} />;
+      })} */}
 
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.salary}</h1>
-      <h1>{props.position}</h1>
-      <h1>{props.company}</h1>
-    </div>
-  );
-}
-
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
+      {planets.map((planet, index) => {
+        return (
+          <div>
+            { planet.isGasPlanet && <h1>{planet.name}</h1> }
+          </div>
+        );
+      })}
     </div>
   );
 }
