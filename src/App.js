1de280889832055f926bb1ae1f48824971ce5804
captaindeng12138 <captaindeng12138@gmail.com>
2024-06-13 19:00:13 +0100

    clear

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import './App.css';
-import { useState } from 'react';
-import {Text} from './Text';
 
 function App() {
-  const [showText, setShowText] = useState(false);
-
   return (
     <div className="App">
-      <button onClick={() => {
-        setShowText(!showText);
-      }}>
-        ShowText
-      </button>
-
-      {showText && <Text />}
     </div>
   );
 }
