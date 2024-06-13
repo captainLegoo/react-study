1d6aab8abc29c041879351b27e3fb31c3d83e05c
captaindeng12138 <captaindeng12138@gmail.com>
2024-06-13 13:31:36 +0100

    6.useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import { useState } from 'react';
+import {Text} from './Text';
 
 function App() {
+  const [showText, setShowText] = useState(false);
+
   return (
     <div className="App">
+      <button onClick={() => {
+        setShowText(!showText);
+      }}>
+        ShowText
+      </button>
+
+      {showText && <Text />}
     </div>
   );
 }
