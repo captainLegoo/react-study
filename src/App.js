dd15df2285c9b527251f08f8c952e2fbef964439
captaindeng12138 <captaindeng12138@gmail.com>
2024-06-15 10:45:54 +0100

    clear

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-import { Navbar } from './Navbar';
-import { Contact } from './pages/Contact';
-import { Home } from './pages/Home';
-import { Profile } from './pages/Profile';
-import { useState } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Form } from './components/Form';
 
 function App() {
-  const client = new QueryClient({defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    }
-  }});
 
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='*' element={<h1>404</h1>} />
-          </Routes>
-        </Router>
-      </QueryClientProvider>
+      <Form />
     </div>
   );
 }
