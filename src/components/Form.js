5a814619374504f8c9aeca75251a41db4d37be9f
captaindeng12138 <captaindeng12138@gmail.com>
2024-06-15 11:23:59 +0100

    '11.react-hook-form'

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,41 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
 export const Form = () => {
+
+  const schema = yup.object().shape({
+    fullName: yup.string().required('Your Full Name is Required'),
+    email: yup.string().email().required('Your Email is Required'),
+    age: yup.number().positive().integer().min(18).required('Your Age is Required'),
+    password: yup.string().min(4).max(20).required(),
+    confirmPassword: yup.
+      string()
+      .oneOf([yup.ref('password'), null], 'Passwords must match')
+      .required(),
+  });
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="Full Name..."/>
-      <input type="text" placeholder="Email..."/>
-      <input type="text" placeholder="Age..."/>
-      <input type="password" placeholder="Password..."/>
-      <input type="password" placeholder="Confirm Password..."/>
-      <input type="submit"/>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Full Name..." {...register('fullName')} />
+      <p>{errors.fullName?.message}</p>
+      <input type="text" placeholder="Email..." {...register('email')} />
+      <p>{errors.email?.message}</p>
+      <input type="number" placeholder="Age..." {...register('age')} />
+      <p>{errors.age?.message}</p>
+      <input type="password" placeholder="Password..." {...register('password')} />
+      <p>{errors.password?.message}</p>
+      <input type="password" placeholder="Confirm Password..." {...register('confirmPassword')} />
+      <p>{errors.confirmPassword?.message}</p>
+      <input type="submit" />
     </form>
   );
 }
