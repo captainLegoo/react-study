6b450bae93869ec36c16d39459a3f1307781721b
captaindeng12138 <captaindeng12138@gmail.com>
2024-06-16 19:47:08 +0100

    'clear'

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,22 +1,9 @@
 import './App.css';
-import { Person, Country } from './components/Person';
 
 function App() {
-
-  const getAge = (name: string): number => {
-    return 99;
-  };
-
   return (
     <div className="App">
-      <Person 
-        name={"Kyle"}
-        email="kyle@me.com"
-        age={24}
-        isMarried={true}
-        friends={['Bill', 'Jeff']}
-        country={Country.Brazil}
-      />
+      
     </div>
   );
 }
