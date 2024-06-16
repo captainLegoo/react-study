ecaebea7b0b46666652057527e0a53888693cacd
captaindeng12138 <captaindeng12138@gmail.com>
2024-06-16 14:46:39 +0100

    clear

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
-import { Cat } from './Cat';
-import { useCount } from './useCount';
-
 
 function App() {
 
-  const [countData, addOne, substractOne, resetToZero] = useCount();
-
   return (
     <div className="App">
-      <button onClick={addOne}>Add</button>
-      <button onClick={substractOne}>Substract</button>
-      <button onClick={resetToZero}>Reset to 0</button>
-      <p>{countData}</p>
     </div>
   );
 }
