35cb8be5021b6299bc0d3476a71b06724da8fe07
captaindeng12138 <captaindeng12138@gmail.com>
2024-07-07 16:48:05 +0100

    Navbar

--- dashboard/src/components/Sidebar.jsx
+++ dashboard/src/components/Sidebar.jsx
@@ -10,7 +10,13 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
 
-  const { activeMenu, setActiveMenu} = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize} = useStateContext();
+
+  const handleCloseSizeBar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  }
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-grap-700 dark:text-grap-200 dark:hover:text-black hover:bg-light-gray m-2';
@@ -19,7 +25,7 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to='/' onClick={() => setActiveMenu(false)} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
+          <Link to='/' onClick={() => {handleCloseSizeBar()}} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
             <SiShopware /> <span>Shoppy</span>
           </Link>
           <TooltipComponent content='Menu' position='BottomCenter'>
@@ -38,7 +44,7 @@ const Sidebar = () => {
                 <NavLink
                   to={`/${link.name}`}
                   key={link.name}
-                  onClick={() => { }}
+                  onClick={() => {handleCloseSizeBar()}}
                   className={({ isActive }) => isActive ? activeLink : normalLink}
                 >
                   {link.icon}
