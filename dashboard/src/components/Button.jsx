84f35634ca3a4864bf69f940014b8872f21aec9c
captaindeng12138 <captaindeng12138@gmail.com>
2024-07-15 13:10:24 +0100

    homepage

--- dashboard/src/components/Button.jsx
+++ dashboard/src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
   return (
-    <div>Button</div>
+    <button
+      type='button'
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={`text=${text} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
   )
 }
 
